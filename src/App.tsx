898566e63b7620f06b70ec6b81e71497a161f847
paulomarquess <paulinho_marques2012@hotmail.com>
2022-06-21 09:18:30 -0300

    feat: uma das formas de fazer a requicição com o apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,26 @@
+import { gql } from "@apollo/client";
+import { useEffect } from "react";
+import { client } from "./lib/apollo";
+
 function App() {
+  const GET_LESSONS_QUERY = gql`
+    query {
+      lessons {
+        id
+        title
+      }
+    }
+  `;
+
+  useEffect(() => {
+    client
+      .query({
+        query: GET_LESSONS_QUERY,
+      })
+      .then((response) => {
+        console.log(response.data);
+      });
+  }, []);
   return (
     <div>
       <h1 className="text-5xl font-bold text-violet-500">Hello World</h1>
